99a57ba2c39191d626b505c2dc590b45a3b6aad1
denis <denis@begom.com>
2024-09-18 10:43:36 +0200

    add Increment

--- map.go
+++ map.go
@@ -43,19 +43,49 @@ func (m *Map[K, T]) Set(key K, value T) {
 	m.ver++
 }
 
-//func (m *Map[K,T]) Increment(key K, val T) T {
-//	m.mx.Lock()
-//	defer m.mx.Unlock()
-//	if m.vals == nil {
-//		m.vals = map[K]T{}
-//	}
-//	if v, ok := m.vals[key]; ok {
-//		val += v
-//	}
-//	m.vals[key] = val
-//	m.ver++
-//	return val
-//}
+func (m *Map[K, T]) Increment(key K, val T) T {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+	if m.vals == nil {
+		m.vals = map[K]T{}
+	}
+	if v, ok := m.vals[key]; ok {
+		val = add(val, v).(T)
+	}
+	m.vals[key] = val
+	m.ver++
+	return val
+}
+
+func add(a, b any) (s any) {
+	switch a.(type) {
+	case int:
+		return a.(int) + b.(int)
+	case int8:
+		return a.(int8) + b.(int8)
+	case int16:
+		return a.(int16) + b.(int16)
+	case int32:
+		return a.(int32) + b.(int32)
+	case int64:
+		return a.(int64) + b.(int64)
+	case uint:
+		return a.(uint) + b.(uint)
+	case uint8:
+		return a.(uint8) + b.(uint8)
+	case uint16:
+		return a.(uint16) + b.(uint16)
+	case uint32:
+		return a.(uint32) + b.(uint32)
+	case uint64:
+		return a.(uint64) + b.(uint64)
+	case float32:
+		return a.(float32) + b.(float32)
+	case float64:
+		return a.(float64) + b.(float64)
+	}
+	return a
+}
 
 func (m *Map[K, T]) Delete(key K) {
 	m.mx.Lock()
